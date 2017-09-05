75a77d422803d8277e29d13f106f96c3b3d47bd5
Sergio Flores Labra <sflores@Sergios-MacBook-Pro.local>
2017-09-05 20:29:57 -0300

    validacion cruzada

--- knn/main.py
+++ knn/main.py
@@ -29,42 +29,63 @@ def GUI( ):
 
 def browse( ):
 	file_name = ventana.askopenfile((("CSV", "*.csv"),("All files", "*.*")))
-	test(file_name, True)
+	test(file_name, True, kMax, sMax)
 
 def readFile(filename, delimiter):
 	aux = open(filename).read()
 	aux = [item.split(delimiter) for item in aux.split('\n')[:-1]]
 	return aux
 
-def test(database, plot):
+def test(database, plot, kNum, sNum):
 	#leemos la base de datos
 	iris = readFile(database, ",") 
 	examples = len(iris)
 	columns = len(iris[0])
 
 	#desordenamos los datos
+	#desordenamos los datos
 	indexes = np.random.permutation(examples)
 	iris = np.asarray(iris)[indexes].tolist()
 
-	#dividimos los datos en 90% y 10%
-	train = iris[:int(examples*0.9)]
-	test = iris[int(examples*0.9):examples]
+	elemSeg =  len(iris)/sMax
+	segmentos = [iris[int(i*elemSeg):int(i*elemSeg+elemSeg)] for i in range(sMax)]
+	
+	#probamos para k desde 1 a 10.
+	for k in range(1,kNum+1): #el ultimo termino es exclusivo
+	    accuracy = 0
+	    for i in range(sMax): 
+	    	#seleccionamos los grupos de entrenamiento y los de prueba.
+	    	#se hará 10 veces, para generar una evaluacion cruzada
+	    	index = np.delete([x for x in range(sMax)], i)
+	    	train = np.concatenate(np.asarray(segmentos)[index].tolist())
+	    	test = segmentos[i]
 
-	for k in range(1,11): #el ultimo termino es exclusivo
-		knn = KNN(train,test,k) #train data, test data, k value
-		knn.predict()
-		if(plot):
-			ventana.createLabel( "Accurracy: " + str(knn.accuracy()) + " k: " + str(k) )
-		else:
-			print("Accuracy: " , knn.accuracy(), "k: " , k)
+	    	knn = KNN(train,test,k) #train data, test data, k value
+	    	knn.predict()
+	    	accuracy += knn.accuracy()
+	    accuracy /= sMax
+	    if(plot):
+	    	ventana.createLabel( "Accurracy: " + str(accuracy) + " k: " + str(k) )
+	    else:
+	    	print("Accuracy: " , accuracy, "k: " , k)
 
 #######  Main Program  #######
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-f', metavar='filename', action="store", dest="file", help='database file path name.')
+    parser.add_argument('-k', metavar='k', action="store", dest="k", help='max k number to test (Default = 10)')
+    parser.add_argument('-s', metavar='splits', action="store", dest="s", help='number of segments to validate (Default = 10)')
     args = parser.parse_args()
+    kMax = 10
+    if(args.k != None):
+    	kMax= int(args.k)
+
+    sMax = 10
+    if(args.s != None):
+    	sMax= int(args.s)
+
     if(args.file == None):
     	ventana = GUI()
     	ventana.keepAlive()
     else:
-    	test(args.file, False)
+    	test(args.file, False, kMax, sMax)
